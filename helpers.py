76ed801c53935598a25fe58f6f1b8dc196009f34
nictru <nictru32@gmail.com>
2023-05-31 14:41:48 +0200

    Implement border methods

---
+++ helpers.py
@@ -0,0 +1,32 @@
+def borders(word: str):
+    """
+    Calculates all borders of a word.
+    """
+
+    borders = []
+
+    for i in range(len(word)):
+        substring = word[:i + 1]
+
+        if substring == word[-len(substring):]:
+            borders.append(substring)
+    
+    return borders
+
+def real_borders(word: str):
+    """
+    Finds the real borders of a word.
+    """
+
+    return [border for border in borders(word) if len(border) < len(word)]
+
+def actual_border(word: str):
+    """
+    Finds the actual border of a word.
+    """
+
+    return max(real_borders(word), key=len)
+
+
+if __name__ == "__main__":
+    print(actual_border("aabaabaa"))
