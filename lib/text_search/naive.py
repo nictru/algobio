8c6499408eb3ef40e39ce8fc675027d4c7d8d172
nictru <nictru32@gmail.com>
2023-05-31 14:29:44 +0200

    Implement naive string search

---
+++ lib/text_search/naive.py
@@ -0,0 +1,13 @@
+def naive(text: str, pattern: str) -> int:
+    """
+    Naive string search algorithm
+    """
+    n = len(text)
+    m = len(pattern)
+    for i in range(n - m + 1):
+        j = 0
+        while j < m and text[i + j] == pattern[j]:
+            j += 1
+        if j == m:
+            return i
+    return -1
